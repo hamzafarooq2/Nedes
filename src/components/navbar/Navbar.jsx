0d4d1833e2ece5f855ec5f404ac1a1285e76d605
Your Name <your.email@example.com>
2024-07-07 15:36:30 +0500

    testing

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import "./navbar.css";
 
 function Navbar() {
-  return <div className="nedes__navbar">Navbar</div>;
+  return <div className="nedes__navbar">Navbar Testing</div>;
 }
 
 export default Navbar;
